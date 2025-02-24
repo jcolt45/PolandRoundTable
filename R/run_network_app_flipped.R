7caea13d4f42c48f8069c2bb03b65744cf38ac9a
jcolt45 <jcolt@calpoly.edu>
2025-02-23 19:36:46 -0800

    TEST

--- R/run_network_app_flipped.R
+++ R/run_network_app_flipped.R
@@ -953,13 +953,13 @@ run_network_app_flipped <- function() {
 
       metric_df <- reactive({
 
-        dat <- all_metrics_df() %>%
-          filter(from %in% input$org_lines,
-                 Start.Date <= last_date_2(),
-                 End.Date >= first_date_2()) %>%
-          left_join(organization_meta_info, by = c("from", "Org.ID")) #%>%
+        dat <- all_metrics_df() #%>%
+          #filter(from %in% input$org_lines,
+                 #Start.Date <= last_date_2(),
+                 #End.Date >= first_date_2()) %>%
+          #left_join(organization_meta_info, by = c("from", "Org.ID")) #%>%
           #left_join(member_meta_info)
-
+        print(dat)
         dat$Selected.Metric = dat[[input$metric]]
 
         dat

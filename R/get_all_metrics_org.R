44ae97754af95e4fd023d01278de804d9ebfb4aa
jcolt45 <jcolt@calpoly.edu>
2025-02-23 19:39:06 -0800

    removed print

--- R/get_all_metrics_org.R
+++ R/get_all_metrics_org.R
@@ -116,7 +116,7 @@ get_one_metrics_step_org <- function(affils_by_date,
                                    start = start,
                                    end = end,
                                    min_cons)
-  print(edgelist)
+
   if (is.null(edgelist)) {
     res <- rep(NA, length(orgs))
     names(res) <- orgs
